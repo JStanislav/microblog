55d2cd4daecbb6eb226aca4946bd0405ddd7ec6b
Joaquín <jstaneck@outlook.com>
2020-09-24 21:30:15 -0300

    agregada funcionalidad de api/users

--- app/__init__.py
+++ app/__init__.py
@@ -15,6 +15,7 @@ import os
 from redis import Redis
 import rq
 
+
 db = SQLAlchemy()
 migrate = Migrate()
 login = LoginManager()
@@ -44,6 +45,9 @@ def create_app(config_class=Config):
     app.redis = Redis.from_url(app.config['REDIS_URL'])
     app.task_queue = rq.Queue('microblog-tasks', connection=app.redis)
     
+
+    from app.api import bp as api_bp
+    app.register_blueprint(api_bp, url_prefix='/api')
 
     from app.errors import bp as errors_bp
     app.register_blueprint(errors_bp)
